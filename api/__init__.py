0ab015e3ae44f57a08c72dbdc0e994bbd685c0db
josue-acosta <hannibalsway001@gmail.com>
2020-10-25 14:43:12 -0500

    initial commit

---
+++ api/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+
+# app config
+app = Flask(__name__)
+
+
+# register blueprints
+from api.generic_urls.views import generic_urls_blueprint
+app.register_blueprint(generic_urls_blueprint, url_prefix='/v1/api')
---
+++ api/app.py
@@ -0,0 +1,7 @@
+# A very simple Flask Hello World app for you to get started with...
+from api import app
+
+
+# run settings
+if __name__ == '__main__':
+    app.run()
