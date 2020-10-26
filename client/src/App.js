126d4fb0058a16bd2c5d7730536eb13e984309ab
josue-acosta <hannibalsway001@gmail.com>
2020-10-26 11:18:45 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home'
 
 function App() {
     const [currentTime, setCurrentTime] = useState(0);
@@ -14,12 +17,24 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>It's' {currentTime}.</p>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div style={{ padding: "1rem" }}>
+                <Link style={{ padding: "1rem" }} to="/">App</Link>
+                <Link style={{ padding: "1rem" }} to="/home">Home</Link>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <div className="App">
+                        <header className="App-header">
+                            <strong>APP</strong>
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <p>It's' {currentTime}.</p>
+                        </header>
+                    </div>
+                </Route>
+                <Route path="/home" component={Home} />
+            </Switch>
+        </BrowserRouter>
     );
 }
 
---
+++ client/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <strong>Home</strong>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+export default App;
